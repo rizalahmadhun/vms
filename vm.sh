83e1142609712bfd038f27888b44bef694897e33
rizalahmadhun <razzahmad@outlook.co.id>
2025-11-19 12:39:35 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -373,13 +373,11 @@ start_vm() {
             -cpu "EPYC-Milan,+aes,+avx2,+avx512f,+invtsc,+topoext,+ibpb"
             -drive "file=$IMG_FILE,format=qcow2,if=virtio"
             -drive "file=$SEED_FILE,format=raw,if=virtio"
-            -device "virtio-net-pci,netdev=net0,mq=on,vectors=16"
             -boot order=c
             -global "kvm-pit.lost_tick_policy=discard" 
             -no-hpet
             -rtc "base=utc,clock=host" 
             -device "virtio-rng-pci,rng=rng0"
-            -device "virtio-balloon-pci"
             -netdev "user,id=n0,hostfwd=tcp::$SSH_PORT-:22"
         )
 
